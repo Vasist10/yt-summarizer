e3ed676eda03994d262170b3c3ed45e848bb5fd9
Vasist10 <155972527+Vasist10@users.noreply.github.com>
2025-05-13 21:27:47 +0530

    syntax error fix

--- backend/main.py
+++ backend/main.py
@@ -38,4 +38,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  
-    uvicorn.run(host="0.0.0.0", port=port)
+    uvicorn.run(app,host="0.0.0.0", port=port)
