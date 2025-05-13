e00507f10fcd23d86861b32f91a9ad6c28053522
Vasist10 <155972527+Vasist10@users.noreply.github.com>
2025-05-13 21:39:28 +0530

    uvicorn fix

--- backend/main.py
+++ backend/main.py
@@ -37,5 +37,4 @@ def index():
     return render_template("index.html", summary=summary)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  
-    uvicorn.run(app,host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
