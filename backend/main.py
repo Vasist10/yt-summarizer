9192df8e8b3a92e9db09b30f7121ea8dc663bd1a
Vasist10 <155972527+Vasist10@users.noreply.github.com>
2025-05-13 21:54:47 +0530

    gunicorn

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from transcribe import transcribe_audio
 from summarize_hf import summarize_with_huggingface
 from dotenv import load_dotenv
 from app import app
-import uvicorn
+
 import os
 load_dotenv()
 
@@ -37,4 +37,5 @@ def index():
     return render_template("index.html", summary=summary)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
